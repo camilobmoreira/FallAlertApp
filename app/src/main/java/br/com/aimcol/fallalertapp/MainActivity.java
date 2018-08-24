cd11a4c7129dd9f4bd36016365c83b54f003ea27
camilobmoreira <camilo.moreira@fatec.sp.gov.br>
2018-08-24 15:58:34 -0300

    Creating app with AndroidStudio

---
+++ app/src/main/java/br/com/aimcol/fallalertapp/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.aimcol.fallalertapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
