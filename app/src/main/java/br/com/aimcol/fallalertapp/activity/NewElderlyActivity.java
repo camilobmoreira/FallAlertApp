e0151ad09161a196692259ca0ce65b60decb2bf7
camilobmoreira <camilo.moreira@fatec.sp.gov.br>
2018-10-01 16:59:32 -0300

    Setting elderly name on screen when loading from saved user

--- app/src/main/java/br/com/aimcol/fallalertapp/activity/NewElderlyActivity.java
+++ app/src/main/java/br/com/aimcol/fallalertapp/activity/NewElderlyActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.google.android.gms.common.util.CollectionUtils;
@@ -38,6 +39,12 @@ public class NewElderlyActivity extends AppCompatActivity {
             String elderlyJson = intent.getStringExtra(Elderly.ELDERLY_JSON);
             this.elderly = this.gson.fromJson(elderlyJson, Elderly.class);
         }
+
+        EditText nameEditText = super.findViewById(R.id.name_edit_text);
+        nameEditText.setText(this.elderly.getName());
+
+//        EditText birthEditText = super.findViewById(R.id.birth_edit_text);
+//        birthEditText.setText(this.elderly.getBirthDate());
 
         Button addNewCaregiverButton = super.findViewById(R.id.add_caregiver_button);
         addNewCaregiverButton.setOnClickListener(new View.OnClickListener() {
